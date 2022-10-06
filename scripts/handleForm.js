3f502e91714cbf19ac16646d2040342634ecf97c
Samuel <samuelschanderl@Samuels-MacBook-Pro.local>
2022-10-06 20:51:32 +0200

    finished certificate

--- scripts/handleForm.js
+++ scripts/handleForm.js
@@ -4,61 +4,73 @@ const canvas = document.querySelector('#canvas')
 // Form fields
 const form = document.querySelector('#form');
 const formContainer = document.querySelector('.form-container');
-// const fullName = document.querySelector('#full-name');
-const fullName = "Schanderl"
-// const courseName = document.querySelector('#course-choice').value;
-const courseName = "Baking Essentials"
+
+const generateBtn = document.querySelector('#generate-btn');
+const downloadBtn = document.querySelector('#download-btn');
+const loadingSpinner = document.querySelector('#loader');
+let showSpinner = false;
 
 // canvas context
 const ctx = canvas.getContext('2d');
 
-const image = new Image()
-image.src = "https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/2149455413/settings_images/xr0o5dSoTV2TK0jbbdHW_certificate.png"
-image.onload = function() {
-    drawImage()
-}
+// create image and load it
+const image = new Image();
+image.crossOrigin = 'anonymous';
+image.src = '/img/certificate.png'
 
+// listen for input
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    toggleSpinner();
+    setTimeout(() => {
+        drawImage();
+        toggleSpinner();
+        //make download button and canvas element visible
+        downloadBtn.style.display = 'block';
+        canvas.style.display = 'block';
+    }, 1500)
+})
+
+// listen for download button
+downloadBtn.addEventListener('click', (e) => {
+    const fullName = document.querySelector('#full-name').value;
+    const fullNameFormatted = fullName.replaceAll(' ', '-')
+    // create a link of the canvas image so it can be downloaded
+    downloadBtn.href = canvas.toDataURL();
+    downloadBtn.download = `Certificate-${fullNameFormatted}-${courseName}`;
+})
+
+// insert the certificate image and add the text
 function drawImage() {
+    const fullName = document.querySelector('#full-name').value;
+    const courseName = document.querySelector('#course-choice').value;
+    const courseNameFormatted = courseName.replaceAll('-', ' ');
+    
+
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
 
     // write student name text
-    ctx.font = '26px arial';
-    ctx.fillStyle = '#494949';
-    const courseName = "Baking Essentials"
-    
-    // get the right width depending on text length
+    ctx.font = '70px Poppins';
+    ctx.fillStyle = '#696969';
     const nameWidth = ctx.measureText(fullName).width;
     const nameStartWidth = (canvas.width - nameWidth) / 2;
 
-    ctx.fillText(fullName, nameStartWidth, 157);
+    ctx.fillText(fullName, nameStartWidth, 670);
 
     // write course name
-    ctx.font = '22px arial';
-    ctx.fillStyle = '#494949';
-    const courseWidth = ctx.measureText(courseName).width;
+    ctx.font = '65px Poppins';
+    ctx.fillStyle = '#696969';
+    const courseWidth = ctx.measureText(courseNameFormatted).width;
     const courseStartWidth = (canvas.width - courseWidth) / 2;
-    ctx.fillText(courseName, courseStartWidth, 210);
+    ctx.fillText(courseNameFormatted, courseStartWidth, 920);
 
     // write the date name
-    ctx.font = '16px arial';
-    ctx.fillStyle = '#494949';
-    ctx.fillText(date, 290, 270);
+    ctx.font = '45px Poppins';
+    ctx.fillStyle = '#696969';
+    ctx.fillText(date, 1060, 1150);
 }
 
-// event listener
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault()
-
-//     // pass the parameters to the certificate url
-//     var url = new URL("http://127.0.0.1:5501/certificate.html?")
-
-//     const fullName = firstName.value + "-" + lastName.value
-
-//     window.location.href = `${url}name=${fullName}&course=${courseName}`
-
-// })
-
-// listen for input
-fullName.addEventListener('input', () => {
-    drawImage()
-})
+function toggleSpinner() {
+    showSpinner = !showSpinner
+    showSpinner ? loadingSpinner.style.display = 'block' : loadingSpinner.style.display = 'none';
+}
